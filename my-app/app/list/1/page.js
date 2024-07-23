b562c0bf374a30f6fbb789f2ca6aaae58f0f04e1
gimujeongsig <dbehcuwjc@gmail.com>
2024-07-23 15:30:16 +0900

    7_23

---
+++ my-app/app/list/1/page.js
@@ -0,0 +1,7 @@
+export default function product() {
+    return (
+        <div>
+            <h4>상품 2</h4>
+        </div>
+    )
+    }
---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  let name = 'kim jeong sig'
+  return (
+    <div>
+      
+      <h4 className="title" style={{color:'red'}}>충주상고</h4>
+      <p className="title-sub">by dev {name}</p>
+    </div>
+  );
+}
